93341093775e2eb59285580600811bbfce8ff7e7
snellefiets <snellefietsX@outlook.com>
2020-11-14 11:32:13 +0100

    initial commit

---
+++ src/main/java/com/day/javasample/JavasampleApplication.java
@@ -0,0 +1,13 @@
+package com.day.javasample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavasampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavasampleApplication.class, args);
+	}
+
+}
