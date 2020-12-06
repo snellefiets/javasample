1d2e69cd9f148ad20e5085cb07e869c58a062d97
snellefiets <snellefietsX@outlook.com>
2020-12-06 13:38:14 +0100

    moved JavaCodeSamples in own module

---
+++ samplecode/src/test/java/samplecode/SamplecodeApplicationTests.java
@@ -0,0 +1,13 @@
+package samplecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamplecodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- samplecode/src/test/java/samplecode/future/FutureTest.java
+++ samplecode/src/test/java/samplecode/future/FutureTest.java
@@ -1,4 +1,4 @@
-package com.day.javasample.future;
+package samplecode.future;
 
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
