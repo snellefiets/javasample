97d647e0e8a5e5fc4e8a60f72e8a736e4368f746
snellefiets <snellefietsX@outlook.com>
2020-12-06 15:00:09 +0100

    roverkata: turn left refactoring

--- marsrover/src/main/java/kata/marsrover/Rover.java
+++ marsrover/src/main/java/kata/marsrover/Rover.java
@@ -7,20 +7,26 @@ public class Rover {
     private String direction = "N";
 
     public String execute(String command) {
-        if ("L".equals(command)) {
-            direction = "W";
-        } else if ("LL".equals(command)) {
-            direction = "S";
-        } else if ("LLL".equals(command)) {
-            direction = "E";
-        } else if ("LLLL".equals(command)) {
-            direction = "N";
-        } else if ("LLLLL".equals(command)) {
-            direction = "W";
+        if (command.startsWith("L")) {
+            for (int i = 0; i < command.length(); i++) {
+                turnLeft();
+            }
         } else {
             yPosition += command.length();
         }
 
         return String.format("%d-%d- %s", xPosition, yPosition, direction);
+    }
+
+    private void turnLeft() {
+        if ("N".equals(direction)) {
+            direction = "W";
+        } else if ("W".equals(direction)) {
+            direction = "S";
+        } else if ("S".equals(direction)) {
+            direction = "E";
+        } else if ("E".equals(direction)) {
+            direction = "N";
+        }
     }
 }
