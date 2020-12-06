ea8c83cb26d921143bea07dc7fc7cb068a657753
snellefiets <snellefietsX@outlook.com>
2020-12-06 14:30:06 +0100

    roverkata: start at 0-0- N

--- marsrover/src/main/java/kata/marsrover/Rover.java
+++ marsrover/src/main/java/kata/marsrover/Rover.java
@@ -1,4 +1,7 @@
 package kata.marsrover;
 
 public class Rover {
+    public String execute(String command) {
+        return "0-0- N";
+    }
 }
--- marsrover/src/test/java/kata/marsrover/RoverTest.java
+++ marsrover/src/test/java/kata/marsrover/RoverTest.java
@@ -1,7 +1,21 @@
 package kata.marsrover;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class RoverTest {
 
     private Rover rover;
 
+    @BeforeEach
+    void setUp() {
+        rover = new Rover();
+    }
+
+    @Test
+    void should_start_at_0_0_N() {
+        assertThat(rover.execute("")).isEqualTo("0-0- N");
+    }
 }
