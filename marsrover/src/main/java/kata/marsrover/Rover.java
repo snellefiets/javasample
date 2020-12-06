de3700cf51b021e1065c1a233ddf0e6c9ec964ce
snellefiets <snellefietsX@outlook.com>
2020-12-06 14:44:50 +0100

    roverkata: refactored move

--- marsrover/src/main/java/kata/marsrover/Rover.java
+++ marsrover/src/main/java/kata/marsrover/Rover.java
@@ -1,16 +1,14 @@
 package kata.marsrover;
 
 public class Rover {
-    public String execute(String command) {
-        if ("M".equals(command))
-            return "0-1- N";
-        else if ("MM".equals(command))
-            return "0-2- N";
-        else if ("MMM".equals(command))
-            return "0-3- N";
-        else if ("MMMM".equals(command))
-            return "0-4- N";
 
-        return "0-0- N";
+    private int xPosition = 0;
+    private int yPosition = 0;
+    private String direction = "N";
+
+    public String execute(String command) {
+        yPosition += command.length();
+
+        return String.format("%d-%d- %s", xPosition, yPosition, direction);
     }
 }
