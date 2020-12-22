6e285dad42b41b58bb792de3cd26306a2ed93fa4
snellefiets <snellefietsX@outlook.com>
2020-12-22 15:47:52 +0100

    exercises: StudentHelperP2Test removed comments

--- exercises/src/test/java/com/exercise/student/StudentHelperP2Test.java
+++ exercises/src/test/java/com/exercise/student/StudentHelperP2Test.java
@@ -66,27 +66,4 @@ public class StudentHelperP2Test {
 
     }
 
-//    @Test
-//    public void testGetGrade() {
-//
-//        assertEquals("A", helper.getGrade(99, false));
-//        assertEquals("A", helper.getGrade(91, false));
-//        assertEquals("B", helper.getGrade(85, false));
-//        assertEquals("B", helper.getGrade(51, false));
-//        assertEquals("C", helper.getGrade(50, false));
-//        assertEquals("C", helper.getGrade(45, false));
-//
-//        assertEquals("A", helper.getGrade(99, true));
-//        assertEquals("A", helper.getGrade(96, true));
-//        assertEquals("B", helper.getGrade(89, true));
-//        assertEquals("B", helper.getGrade(56, true));
-//        assertEquals("C", helper.getGrade(55, true));
-//        assertEquals("C", helper.getGrade(50, true));
-//        assertEquals("C", helper.getGrade(45, true));
-//
-//        //Will fail due to wrong implementation
-//        //assertEquals("B",helper.getGrade(95, true));
-//        //assertEquals("B",helper.getGrade(90, false));
-//    }
-
 }
