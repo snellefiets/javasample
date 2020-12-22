5dffae6059100781c2ac1bcef7336007561d9543
snellefiets <snellefietsX@outlook.com>
2020-12-22 11:17:48 +0100

    exercises: refactored StudentHelperTest

--- exercises/src/test/java/com/exercise/student/StudentHelperTest.java
+++ exercises/src/test/java/com/exercise/student/StudentHelperTest.java
@@ -1,14 +1,37 @@
 package com.exercise.student;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class StudentHelperTest {
 
-	StudentHelper helper = new StudentHelper();
-	
+	private static final int GRADE_B_COMMON_LOWER_LIMIT = 51;
+	private static final int GRADE_B_MATH_UPPER_LIMIT = 90;
+	private static final int GRADE_B_DEFAULT_VALUE = 66;
+	final StudentHelper helper = new StudentHelper();
+
+	@Nested
+	class isGradeB_should {
+
+		@ParameterizedTest
+		@ValueSource(ints = {GRADE_B_COMMON_LOWER_LIMIT, GRADE_B_DEFAULT_VALUE, GRADE_B_MATH_UPPER_LIMIT})
+		void isGradeB_should_return_true_for_subject_math(int marks) {
+			assertEquals(true, helper.isGradeB(marks, true));
+		}
+
+
+		@ParameterizedTest
+		@ValueSource(ints = {GRADE_B_COMMON_LOWER_LIMIT - 1, GRADE_B_MATH_UPPER_LIMIT + 1})
+		void isGradeB_should_return_false_for_subject_math(int marks) {
+			assertEquals(false, helper.isGradeB(marks, true));
+		}
+	}
+
 	@Test
 	public void testIsGradeB() {
 		assertEquals(false,helper.isGradeB(30, false));
