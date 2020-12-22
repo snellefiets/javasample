1992969b08a31404e2d8be0b2ffd735e43c7498d
snellefiets <snellefietsX@outlook.com>
2020-12-22 11:28:20 +0100

    exercises: refactored StudentHelperTest non math subject

--- exercises/src/test/java/com/exercise/student/StudentHelperTest.java
+++ exercises/src/test/java/com/exercise/student/StudentHelperTest.java
@@ -11,6 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class StudentHelperTest {
 
 	private static final int GRADE_B_COMMON_LOWER_LIMIT = 51;
+	private static final int GRADE_B_COMMON_UPPER_LIMIT = 80;
 	private static final int GRADE_B_MATH_UPPER_LIMIT = 90;
 	private static final int GRADE_B_DEFAULT_VALUE = 66;
 	final StudentHelper helper = new StudentHelper();
@@ -24,11 +25,23 @@ public class StudentHelperTest {
 			assertEquals(true, helper.isGradeB(marks, true));
 		}
 
-
 		@ParameterizedTest
 		@ValueSource(ints = {GRADE_B_COMMON_LOWER_LIMIT - 1, GRADE_B_MATH_UPPER_LIMIT + 1})
 		void return_false_for_subject_math(int marks) {
 			assertEquals(false, helper.isGradeB(marks, true));
+		}
+
+		@ParameterizedTest
+		@ValueSource(ints = {GRADE_B_COMMON_LOWER_LIMIT, GRADE_B_DEFAULT_VALUE, GRADE_B_COMMON_UPPER_LIMIT})
+		void return_true_for_subject_non_math(int marks) {
+			assertEquals(true, helper.isGradeB(marks, false));
+		}
+
+
+		@ParameterizedTest
+		@ValueSource(ints = {GRADE_B_COMMON_LOWER_LIMIT - 1, GRADE_B_COMMON_UPPER_LIMIT + 1})
+		void return_false_for_subject_non_math(int marks) {
+			assertEquals(false, helper.isGradeB(marks, false));
 		}
 	}
 
