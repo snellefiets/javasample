3ed761e326c2faf2ffc7dc1a16ac8f3da110676c
snellefiets <snellefietsX@outlook.com>
2020-12-22 11:41:43 +0100

    exercises: StudentHelper isGradeB enhancement

--- exercises/src/main/java/com/exercise/student/StudentHelper.java
+++ exercises/src/main/java/com/exercise/student/StudentHelper.java
@@ -10,9 +10,8 @@ public class StudentHelper {
 	* You get a grade B if marks are between 51 and 80 (both inclusive). Except for Maths where the upper limit is increased by 10.
 	*/
 	public boolean isGradeB(int marks, boolean isMaths) {
-		final int upperLimit = isMaths ?
-				GRADE_B_UPPER_LIMIT+ MATH_MARKUP :
-				GRADE_B_UPPER_LIMIT;
+		final int markup = isMaths ? MATH_MARKUP : 0;
+		final int upperLimit = GRADE_B_UPPER_LIMIT + markup;
 		return marks>= GRADE_B_LOWER_LIMIT && marks<=upperLimit;
 	}
 
