6cd6283b8ca3a517c049ec21eb327ac9bf70d6c2
snellefiets <snellefietsX@outlook.com>
2020-12-22 15:24:33 +0100

    exercises: StudentHelperP2 enhancement, correction of code

--- exercises/src/main/java/com/exercise/student/StudentHelper_P2.java
+++ exercises/src/main/java/com/exercise/student/StudentHelper_P2.java
@@ -1,36 +1,37 @@
 package com.exercise.student;
-public class StudentHelper_P2 {
 
-	/* PROBLEM 2 */
+public class StudentHelper_P2 {
+    public static final int GRADE_A_LOWER_LIMIT = 91;
+    public static final int GRADE_B_LOWER_LIMIT = 51;
+    public static final int MATH_MARKUP = 5;
+
+    /* PROBLEM 2 */
 	/*
 	You are awarded a grade based on your marks.
 	Grade A = 91 to 100, Grade B = 51 to 90, Otherwise Grade C
 	Except for Maths where marks to get a Grade are 5 higher than required for other subjects.
 	*/
 
-	public String getGrade(int mark, boolean isMaths) {
-		String grade = "C";
-		
-		if (isGradeA(mark, isMaths))
-			grade = "A";
-		else if (isBGrade(mark, isMaths)) {
-			grade = "B";
-		}
-		return grade;
-	}
+    public String getGrade(int mark, boolean isMaths) {
+        if (isGradeA(mark, isMaths))
+            return "A";
+        else if (isGradeB(mark, isMaths))
+            return "B";
+        else
+            return "C";
+    }
 
-	private boolean isGradeA(int mark, boolean isMaths) {
-		int lowerLimitForAGrade = isMaths ? 95
-				: 90;
-		return mark > lowerLimitForAGrade;
-	}
+    private boolean isGradeA(int mark, boolean isMaths) {
+        final int lowerLimitGradeA_maths = GRADE_A_LOWER_LIMIT + MATH_MARKUP;
+        final int lowerLimitGradeA = isMaths ? lowerLimitGradeA_maths : GRADE_A_LOWER_LIMIT;
+        return mark >= lowerLimitGradeA;
+    }
 
-	private boolean isBGrade(int mark, boolean isMaths) {
-		int lowerLimitGradeB = isMaths ? 55
-				: 50;
-		return mark > lowerLimitGradeB && mark < 90;
-	}
-
+    private boolean isGradeB(int mark, boolean isMaths) {
+        final int lowerLimitGradeB_maths = GRADE_B_LOWER_LIMIT + MATH_MARKUP;
+        final int lowerLimitGradeB = isMaths ? lowerLimitGradeB_maths : GRADE_B_LOWER_LIMIT;
+        return mark >= lowerLimitGradeB;
+    }
 
 
 }
