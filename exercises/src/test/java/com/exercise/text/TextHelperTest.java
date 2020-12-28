ced14f6a4f8c9d30ccdfb6eaaf90cd3436fab9a9
snellefiets <snellefietsX@outlook.com>
2020-12-28 17:12:28 +0100

    exercises: improved TextHelperTest

--- exercises/src/test/java/com/exercise/text/TextHelperTest.java
+++ exercises/src/test/java/com/exercise/text/TextHelperTest.java
@@ -2,28 +2,29 @@ package com.exercise.text;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TextHelperTest {
 
-	TextHelper helper = new TextHelper();
-	
-	@Test
-	public void testSwapLastTwoCharacters() {
-		assertEquals("",helper.swapLastTwoCharacters(""));
-		assertEquals("A",helper.swapLastTwoCharacters("A"));
-		assertEquals("BA",helper.swapLastTwoCharacters("AB"));
-		assertEquals("RANI",helper.swapLastTwoCharacters("RAIN"));
-	}
+    TextHelper helper = new TextHelper();
 
-	@Test
-	@Disabled
-	public void testTruncateAInFirst2Positions() {
-		assertEquals("",helper.truncateAInFirst2Positions(""));
-		assertEquals("BCD",helper.truncateAInFirst2Positions("ABCD"));
-		assertEquals("CD",helper.truncateAInFirst2Positions("AACD"));
-		assertEquals("BCD",helper.truncateAInFirst2Positions("BACD"));
-		assertEquals("BBAA",helper.truncateAInFirst2Positions("BBAA"));
-	}
+    @ParameterizedTest
+    @CsvSource({",", "A,A", "BA,AB", "RANI,RAIN"})
+    public void testSwapLastTwoCharacters(String given, String expected) {
+        assertThat(helper.swapLastTwoCharacters(given)).isEqualTo(expected);
+    }
+
+    @Test
+    @Disabled
+    public void testTruncateAInFirst2Positions() {
+        assertEquals("", helper.truncateAInFirst2Positions(""));
+        assertEquals("BCD", helper.truncateAInFirst2Positions("ABCD"));
+        assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
+        assertEquals("BCD", helper.truncateAInFirst2Positions("BACD"));
+        assertEquals("BBAA", helper.truncateAInFirst2Positions("BBAA"));
+    }
 }
