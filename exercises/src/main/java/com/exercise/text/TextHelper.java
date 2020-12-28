2b689ed189086aef46ec76c5beeae9b7887d1385
snellefiets <snellefietsX@outlook.com>
2020-12-28 17:15:14 +0100

    exercises: formatting

--- exercises/src/main/java/com/exercise/text/TextHelper.java
+++ exercises/src/main/java/com/exercise/text/TextHelper.java
@@ -2,17 +2,17 @@ package com.exercise.text;
 
 public class TextHelper {
 
-	public String swapLastTwoCharacters(String str) {
-		if (str==null || str.length() < 2)
-			return str;
-		final int stringLength = str.length();
-		final String prefix = str.substring(0, stringLength-2);
-		final String firstChar = str.substring(stringLength-2, stringLength-1);
-		final String secondChar = str.substring(stringLength-1);
-		return prefix + secondChar + firstChar;
-	}
+    public String swapLastTwoCharacters(String str) {
+        if (str == null || str.length() < 2)
+            return str;
+        final int stringLength = str.length();
+        final String prefix = str.substring(0, stringLength - 2);
+        final String firstChar = str.substring(stringLength - 2, stringLength - 1);
+        final String secondChar = str.substring(stringLength - 1);
+        return prefix + secondChar + firstChar;
+    }
 
-	public String truncateAInFirst2Positions(String str) {
-		return null;
-	}
+    public String truncateAInFirst2Positions(String str) {
+        return null;
+    }
 }
