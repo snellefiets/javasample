5a89af8d9faa5a309b204eeefb4141c879e559e7
snellefiets <snellefietsX@outlook.com>
2020-12-28 17:12:55 +0100

    exercises: TextHelper implementation

--- exercises/src/main/java/com/exercise/text/TextHelper.java
+++ exercises/src/main/java/com/exercise/text/TextHelper.java
@@ -3,7 +3,12 @@ package com.exercise.text;
 public class TextHelper {
 
 	public String swapLastTwoCharacters(String str) {
-		return null;
+		if (str==null || str.length()<2)
+			return str;
+		final String prefix = str.substring(0, str.length()-2);
+		final String firstChar = str.substring(str.length()-2, str.length()-1);
+		final String secondChar = str.substring(str.length()-1);
+		return prefix + secondChar + firstChar;
 	}
 
 	public String truncateAInFirst2Positions(String str) {
