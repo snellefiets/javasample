2326edcf59ad60e6e1cc65703e301e9f9cc990a9
snellefiets <snellefietsX@outlook.com>
2021-01-24 17:29:26 +0100

    leapyear: added test values from Instruction Examples

--- leapyear/src/test/java/com/tdd/leapyear/YearServiceTest.java
+++ leapyear/src/test/java/com/tdd/leapyear/YearServiceTest.java
@@ -15,14 +15,14 @@ public class YearServiceTest {
     class isLeapYear_should {
 
         @ParameterizedTest
-        @ValueSource(ints = {2001, 2002, 2003})
+        @ValueSource(ints = {1997, 2001, 2002, 2003})
         void return_false(int givenYear) {
             assertThat(yearService.isLeapYear(givenYear)).isFalse();
         }
 
         @ParameterizedTest
-        @ValueSource(ints = {2004, 2008})
-        void return_true(int givenYear) {
+        @ValueSource(ints = {1996, 2004, 2008})
+        void return_true_dueTo_divisible_by_4(int givenYear) {
             assertThat(yearService.isLeapYear(givenYear)).isTrue();
         }
     }
