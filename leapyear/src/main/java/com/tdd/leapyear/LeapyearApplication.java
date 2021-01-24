035426cbf24094c5403734355354385b6ae7cb4f
snellefiets <snellefietsX@outlook.com>
2021-01-24 16:22:47 +0100

    leapyear: basic structure

---
+++ leapyear/src/main/java/com/tdd/leapyear/LeapyearApplication.java
@@ -0,0 +1,13 @@
+package com.tdd.leapyear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeapyearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeapyearApplication.class, args);
+	}
+
+}
---
+++ leapyear/src/test/java/com/tdd/leapyear/LeapyearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdd.leapyear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeapyearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
