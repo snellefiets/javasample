7ceaa912513a7feecfd6136299b29efbd400ccac
snellefiets <snellefietsX@outlook.com>
2021-01-24 18:00:19 +0100

    leapyear: refactoring

--- leapyear/src/main/java/com/tdd/leapyear/YearService.java
+++ leapyear/src/main/java/com/tdd/leapyear/YearService.java
@@ -2,9 +2,10 @@ package com.tdd.leapyear;
 
 public class YearService {
     public boolean isLeapYear(int year) {
-        if (isDivisible(year, 100) && !isDivisible(year, 400)) return false;
-        if (isDivisible(year, 4)) return true;
-        return false;
+        if (isDivisible(year, 100) && !isDivisible(year, 400))
+            return false;
+        else
+            return isDivisible(year, 4);
     }
 
     private boolean isDivisible(int number, int divisor) {
