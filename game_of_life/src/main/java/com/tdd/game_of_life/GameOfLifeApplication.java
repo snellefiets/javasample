2ee7f7a95eff8a1b72948b9010386a2521da91a2
snellefiets <snellefietsX@outlook.com>
2021-02-27 13:35:29 +0100

    gameOfLife: initial commit

---
+++ game_of_life/src/main/java/com/tdd/game_of_life/GameOfLifeApplication.java
@@ -0,0 +1,13 @@
+package com.tdd.game_of_life;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameOfLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameOfLifeApplication.class, args);
+	}
+
+}
---
+++ game_of_life/src/test/java/com/tdd/game_of_life/GameOfLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdd.game_of_life;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameOfLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
